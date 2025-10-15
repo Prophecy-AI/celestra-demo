a1f2c8dcf4f69ca20d4a97fbc63eb2cdcff5c6b3
Ilia Mireskandari <mrskndri@gmail.com>
2025-10-14 21:48:58 -0400

    fix: Enhance setup-new-runner.sh for Docker installation and environment setup

--- scripts/setup-new-runner.sh
+++ scripts/setup-new-runner.sh
@@ -20,14 +20,26 @@ git lfs install
 git checkout workflow
 
 # Install Docker
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 # Install NVIDIA container toolkit
+wget -nv -O- https://lambda.ai/install-lambda-stack.sh | 
+I_AGREE_TO_THE_CUDNN_LICENSE=1 sh -
+
 sudo apt-get install -y nvidia-container-toolkit
 sudo nvidia-ctk runtime configure --runtime=docker
 sudo systemctl restart docker
@@ -36,10 +48,14 @@ sudo systemctl restart docker
 sudo usermod -aG docker ubuntu
 
 # Setup Python venv and install mlebench
+sudo apt-get install python3-full
 cd ~/canada-research/mle-bench
 python3 -m venv venv
 source venv/bin/activate
 pip install -e .
+
+# setup kaggle.json
+vim ~/.config/kaggle/kaggle.json
 
 # Fix permissions
 sudo chown -R ubuntu:ubuntu ~/.cache 2>/dev/null || true
